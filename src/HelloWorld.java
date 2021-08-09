9268561057d715e77a513e9f227a10eb882ed1e3
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-09 09:57:50 -0500

    update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,14 @@
 public class HelloWorld {
 
    public static void main(String[] args) {
-       System.out.println("Hello, World!");
+//       System.out.println("Hello, World!");
+
+       int myFavoriteNumber;
+       myFavoriteNumber = 21;
+       System.out.println(myFavoriteNumber);
     }
+
+
+
+
 }
