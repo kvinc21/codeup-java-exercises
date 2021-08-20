5a3b8a341b5d158634e98155c03ffe227536e63a
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-20 12:24:00 -0500

    lost code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,69 +1,61 @@
+
 public class HelloWorld {
 
    public static void main(String[] args) {
-//       System.out.println("Hello, World!");
-//
-//       int myFavoriteNumber;
-//       myFavoriteNumber = 21;
-//       System.out.println(myFavoriteNumber);
-//
-//       String myString;
-//       myString = "Hey all";
-//       System.out.println((myString));
-//
-//
-////       Add string line
-//        myString = "Goodbye all";
-//        System.out.println(myString);
-//
-//        myString = "3.14159";
-//        System.out.println(myString);
-//
-////        long myNumber;
-////
-//////        myNumber = 3.14;
-//////        myNumber = 123L;
-////        myNumber = 123;
-////        System.out.println(myNumber);
-//
-//        float myNumber = (float) 3.14;
-//        System.out.println(myNumber);
-//
-//       int x = 5;
-//       System.out.println(x++);
-//       System.out.println(x);
-//
-//       int x = 5;
-//       System.out.println(++x);
-//       System.out.println(x);
-//
-//
-//       String theNumberThree = "three";
-//       Object o = theNumberThree;
-//       int three = (int) o;
-//
-//       int x = 4;
-//       x = x + 5;
-//
-//       x += 5;
+       System.out.println("Hello, World!");
 
+       int myFavoriteNumber;
+       myFavoriteNumber = 21;
+       System.out.println(myFavoriteNumber);
+
+       String myString;
+       myString = "Hey all";
+       System.out.println((myString));
+
+
+//       Add string line
+        myString = "Goodbye all";
+        System.out.println(myString);
+
+        myString = "3.14159";
+        System.out.println(myString);
+
+//        long myNumber;
+
+//        myNumber = 3.14;
+//      myNumber = 123L;
+//       myNumber = 123;
+//       System.out.println(myNumber);
 //
-//       int x = 3;
-//       int y = 4;
-//       y = y * x;
+//     float myNewNumber = (float) 3.14;
+//     System.out.println(myNewNumber);
+//int x = System.out.println(x++System.out.println(x);
+
+//    int y = 5;
+//    System.out.println(++y);
+//    System.out.println(y);
 //
-//       y *= x;
-//       System.out.println(y);
+//
+//    String theNumberThree = "three";
+//    Object o = theNumberThree;
+//    int three = (int) o;
+//
+//    int x = 4;
+//    x = x + 5;
+//
+//    x += 5;
+//
+//
+//    int x = 3;
+//    int y = 4;
+//    y = y * x;
+//
+//    y *= x;
+//    System.out.println(y);
 
 
 
-       int x = 10;
-       int y = 2;
-       x = x / y;
-       y = y - x;
 
-       System.out.println(x);
-       System.out.println(y);
 
 
 
