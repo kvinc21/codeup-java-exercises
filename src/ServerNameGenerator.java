48711fb0be4e60f7946b388ba8d79b094f14c498
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-30 15:43:27 -0500

    update

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,9 +1,11 @@
-
+import java.util.Random
 
 public class ServerNameGenerator {
 
     public static String[] adjectives = {"Charming", "Cruel", "Gentle", "Huge", "Perfect", "Rough", "Sharp", "Tasty", "Zealous"};
     public static String[] nouns = {"bed", "cat", "movie", "train", "country", "book", "phone", "match", "speaker", "clock", "pen", "David", "violin"};
 
-
+    public static void main(String[] args){
+        System.out.println("");
+    }
 }
