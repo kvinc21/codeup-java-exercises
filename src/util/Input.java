9c7086ddcf727a0d1d21090cf996109f3d3ed005
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-30 14:00:04 -0500

    updates

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,26 +1,45 @@
 package util;
 
-impor java.sql.SQLOutput;
+//
 import java.util.Scanner;
-
 
 
 public class Input {
 
     private Scanner scanner;
 
-    public Input(){
+    public Input() {
         this.scanner = new Scanner(System.in);
     }
 
-    public String getString(){
+    public String getString() {
         String userString = scanner.nextLine();
         return userString;
     }
 
-    public boolean yesNo(){
+    public boolean yesNo() {
         String userAnswer = scanner.next();
         scanner.nextLine();
         return userAnswer.equals("y");
     }
+
+    public int getInt(int min, int max) {
+        System.out.println("Give an Integer between" + min + " and " + max + ": ");
+        int userInt;
+
+        try {
+            userInt = Integer.valueOf(this.getString());
+        } catch (NumberFormatException e) {
+            System.out.println("This is not an integer!");
+            return getInt(min, max);
+        }
+
+        if (userInt < min || userInt > max){
+            System.out.println("Invalid input! Try again!");
+            return getInt(min, max);
+        }else {
+            return userInt;
+        }
+    }
+
 }
