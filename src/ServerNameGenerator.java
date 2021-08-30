95f340e46637f63bbd0cadc4f53ad770fbc6b9e7
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-30 15:56:19 -0500

    update

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,11 +1,18 @@
-import java.util.Random
+import java.util.Random;
 
 public class ServerNameGenerator {
 
     public static String[] adjectives = {"Charming", "Cruel", "Gentle", "Huge", "Perfect", "Rough", "Sharp", "Tasty", "Zealous"};
     public static String[] nouns = {"bed", "cat", "movie", "train", "country", "book", "phone", "match", "speaker", "clock", "pen", "David", "violin"};
 
-    public static void main(String[] args){
-        System.out.println("");
+    public static void main(String[] args) {
+        System.out.println("Here is your server name:\n ");
+                System.out.println(getWord(adjectives) + " " + getWord(nouns));
+    }
+
+    public static String getWord(String[] choices){
+        Random random = new Random();
+        int randomNumber = random.nextInt(choices.length);
+        return choices[randomNumber];
     }
 }
