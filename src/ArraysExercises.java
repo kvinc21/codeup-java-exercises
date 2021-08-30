447f1c9b18e948d3a5939953f9e2c0c7510b2bce
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-30 11:56:52 -0500

    import function issue

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,17 +3,17 @@ import java.util.Arrays;
 public class ArraysExercises {
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers);
-        System.out.println(Arrays.toString(numbers));
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers);
+//        System.out.println(Arrays.toString(numbers));
 
-        String[] people = new Person[3];
+        Person[] people = new Person[3];
 
         people[0] = new Person("Ken");
         people[1] = new Person ("Steve");
         people[2] = new Person ("Travis");
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < people.length; i++) {
             System.out.println(people[i].getName());
         }
 
--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,2 +1,9 @@
+
+
 public class ControlFlowExercises {
+    public static void main(String[] args){
+
+
+    }
+
 }
