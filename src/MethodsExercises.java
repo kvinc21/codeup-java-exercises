f2663824588fd049c5824159c057bceaa2956469
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-08-31 18:13:44 -0500

    update

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,36 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class MethodsExercises {
 
+    public static int Addition(int x, int y){
+        return x + y;
+    }
+    public static int Subtraction(int x, int y){
+        return x - y;
+    }
+    public static int Multiplication(int x, int y){
+        return x * y;
+    }
+    public static int Division(int x, int y){
+        return x / y;
+    }
+    public static double Modulus(double x, double y){
+        return x % y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Addition(5, 10));
+        System.out.println(Subtraction(35, 20));
+        System.out.println(Multiplication(5, -3));
+        System.out.println(Division(45, 3));
+        System.out.println(Modulus(45, 30));
+    };
+
+    public static int getInteger(int min, int max){
+        Scanner scan = new Scanner(System.in);
+
+
+    }
 }
 
